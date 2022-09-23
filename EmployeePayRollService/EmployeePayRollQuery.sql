3d1cdd4e0ad6b711ab81b24bc72a4f12b59944b2
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-09-23 16:56:17 +0530

    Create table

--- EmployeePayRollService/EmployeePayRollQuery.sql
+++ EmployeePayRollService/EmployeePayRollQuery.sql
@@ -2,3 +2,11 @@
 create database EmployeePayRollServices
 
 use EmployeePayRollServices
+-----UC2----
+create table employee_payroll
+(
+Id int IDENTITY(1,1) Primary key,
+Name varchar(50) NOT NULL,
+Salary float NOT NULL,
+Startdate date ,
+);
