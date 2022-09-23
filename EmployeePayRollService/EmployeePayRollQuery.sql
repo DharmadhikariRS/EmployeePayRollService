20a689f0e85e49f37898f6747fef14807f73eb0d
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-09-23 16:58:49 +0530

    insert record in  table

--- EmployeePayRollService/EmployeePayRollQuery.sql
+++ EmployeePayRollService/EmployeePayRollQuery.sql
@@ -10,3 +10,7 @@ Name varchar(50) NOT NULL,
 Salary float NOT NULL,
 Startdate date ,
 );
+-----UC3----
+insert into employee_payroll values('Prabhudas',25000,GETDATE())
+insert into employee_payroll values('Bhanudas',35000,GETDATE())
+insert into employee_payroll values('Amarendra',23000,GETDATE())
