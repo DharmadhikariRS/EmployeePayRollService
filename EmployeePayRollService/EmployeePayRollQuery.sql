fa5047ba7e9254a66de4708afcc3f2713ebe8d48
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-10-01 12:32:19 +0530

    Retriving ER Diagram data

--- EmployeePayRollService/EmployeePayRollQuery.sql
+++ EmployeePayRollService/EmployeePayRollQuery.sql
@@ -128,3 +128,34 @@ insert into EmployeeDepartment values
 (1,3),
 (3,4)
 select * from EmployeeDepartment
+
+-----------------UC12-------------------
+
+--UC 4: Retrieve all Data
+SELECT CompanyID,CompanyName,EmployeeID,EmployeeName,EmployeeAddress,EmployeePhoneNumber,StartDate,Gender,BasicPay,Deductions,TaxablePay,IncomeTax,NetPay,DepartName from Company
+INNER JOIN Employee ON Company.CompanyID = Employee.CompanyIdentity
+INNER JOIN PayrollCalculate on PayrollCalculate.EmployeeIdentity=Employee.EmployeeID
+INNER JOIN EmployeeDepartment on Employee.EmployeeID=EmployeeDepartment.EmployeeIdentity
+INNER JOIN Department on Department.DepartmentId=EmployeeDepartment.DepartmentIdentity 
+
+--UC 5: Select Query using Cast() an GetDate()
+SELECT CompanyID,CompanyName,EmployeeID,EmployeeName,BasicPay,Deductions,TaxablePay,IncomeTax,NetPay from Company
+INNER JOIN Employee ON Company.CompanyID = Employee.CompanyIdentity and StartDate BETWEEN Cast('2012-11-12' as Date) and GetDate()
+INNER JOIN PayrollCalculate on PayrollCalculate.EmployeeIdentity=Employee.EmployeeID
+
+---UC5 Retrieve query based on Name
+SELECT CompanyID,CompanyName,EmployeeID,EmployeeName,BasicPay,Deductions,TaxablePay,IncomeTax,NetPay from Company
+INNER JOIN Employee ON Company.CompanyID = Employee.CompanyIdentity and Employee.EmployeeName='Arjun Pandit'
+INNER JOIN PayrollCalculate on PayrollCalculate.EmployeeIdentity=Employee.EmployeeID
+
+--UC 7: Use Aggregate Functions and Group by Gender
+select Sum(BasicPay) as "TotalSalary",Gender from Employee
+INNER JOIN PayrollCalculate on PayrollCalculate.EmployeeIdentity=Employee.EmployeeID group by Gender;
+select Avg(BasicPay) as "AverageSalary",Gender from Employee
+INNER JOIN PayrollCalculate on PayrollCalculate.EmployeeIdentity=Employee.EmployeeID group by Gender;
+select Min(BasicPay) as "MinimumSalary",Gender from Employee
+INNER JOIN PayrollCalculate on PayrollCalculate.EmployeeIdentity=Employee.EmployeeID group by Gender;
+select Max(BasicPay)  as "MaximumSalary",Gender from Employee
+INNER JOIN PayrollCalculate on PayrollCalculate.EmployeeIdentity=Employee.EmployeeID group by Gender;
+select Count(BasicPay) as "CountSalary",Gender from Employee
+INNER JOIN PayrollCalculate on PayrollCalculate.EmployeeIdentity=Employee.EmployeeID group by Gender;
